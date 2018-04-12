30fefe0e22be73a18c65d770419df8d36e0b327a
Unknown <sushsilsharma.ss451@gmail.com>
2018-04-12 21:02:14 +0530

    removed stub code

--- app.py
+++ app.py
@@ -11,88 +11,62 @@ db = SQLAlchemy(app)
 ma = Marshmallow(app)
 
 
-class User(db.Model):
-    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
-    username = db.Column(db.String(80), unique=True)
-    email = db.Column(db.String(120), unique=True)
+# Assuming there are 50 prime sets
+primes_list = {}
 
-    def __init__(self, username, email):
-        self.username = username
-        self.email = email
+# Database URL
+BASE_URL = "https://volunteer-computing.firebaseio.com/primality-tests/"
 
 
-class UserSchema(ma.Schema):
-    class Meta:
-        # Fields to expose
-        fields = ('username', 'email')
+# endpoint to get the state of primes_list
+@app.route("/get_state", methods=["GET"])
+def get_shared_resource_state():
+    return jsonify(primes_list)
 
 
-user_schema = UserSchema()
-users_schema = UserSchema(many=True)
+# endpoint to get a working URL for the client
+# This will call synchronization problems
+@app.route("/get_working_url", methods=["GET"])
+def user_detail():
+
+    # Find which set is to be checked
+    i = 0
+    for i in range(len(primes_list)):
+        if primes_list[i] == 'yet_to_be_checked':
+            primes_list[i] = 'checking'
+
+    # Return the corresponding URL to download dataset
+    url = BASE_URL+"prime_sets/"+str(i)+"/set/.json"
+
+    return jsonify(url)
 
 
-# Main route
-@app.route("/")
-def main_page():
-    return get_user()
+# endpoint to update prime_list
+# This happens when a client script checks a set of primes
+# and updates the server
+@app.route("/update", methods=["PUT"])
+def user_update():
+
+    # `divisor` can be -1(no divisor found) or a natural number
+    divisor = request.json['divisor']
+    set_number = request.json['set_number']
+
+    if divisor == -1:
+        # update prime list
+        primes_list[set_number] = 'checked'
+    # TODO else do something
+
+    return jsonify("Shared variable updated successfully")
 
 
-# endpoint to create new user
-@app.route("/user", methods=["POST"])
-def add_user():
-    username = request.form['username']
-    email = request.form['email']
-    print(username)
-    print(email)
-    new_user = User(username, email)
-    result = user_schema.dump(new_user)
+# endpoint to reset prime checking list
+@app.route("/reset", methods=["GET"])
+def reset_shared_variable():
 
-    db.session.add(new_user)
-    db.session.commit()
+    for i in range(50):
+        primes_list[i] = 'yet_to_be_checked'
 
-    return jsonify(result)
-
-
-# endpoint to show all users
-@app.route("/user", methods=["GET"])
-def get_user():
-    all_users = User.query.all()
-    result = users_schema.dump(all_users)
-    return jsonify(result)
-
-
-# endpoint to get user detail by ID
-@app.route("/user/<id>", methods=["GET"])
-def user_detail(id):
-    user = User.query.get(id)
-    result = user_schema.dump(user)
-    return jsonify(result)
-
-
-# endpoint to update user
-@app.route("/user/<id>", methods=["PUT"])
-def user_update(id):
-    user = User.query.get(id)
-    username = request.json['username']
-    email = request.json['email']
-
-    user.email = email
-    user.username = username
-
-    db.session.commit()
-    result = user_schema.dump(user)
-    return jsonify(result)
-
-
-# endpoint to delete user
-@app.route("/user/<id>", methods=["DELETE"])
-def user_delete(id):
-    user = User.query.get(id)
-    db.session.delete(user)
-    db.session.commit()
-
-    result = user_schema.dump(user)
-    return jsonify(result)
+    return jsonify("Shared variable reset successfully")
 
 
 if __name__ == '__main__':
